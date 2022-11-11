af5fabe2a6ee6b4c7eb3066dd94ce19b0120ad11
Martin Zanoni <mza@relewise.com>
2022-11-11 07:58:45 +0100

    first try at integration tests

---
+++ tests/php/integration/TrackerTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Relewise\Tests\Integration;
+
+use \PHPUnit\Framework\TestCase;
+use Relewise\Tests\DotEnv;
+use Relewise\Tracker;
+
+class TrackerTest extends TestCase
+{
+    public function testProductView(): void 
+    {
+        DotEnv::load();
+        $datasetId = getenv('DATASET_ID', true) ?: $_ENV['DATASET_ID'];
+        $apiKey = getenv('API_KEY', true) ?: $_ENV['API_KEY'];
+        
+        $tracker = new Tracker($datasetId, $apiKey);
+
+        $response = $tracker->trackProductView("p-1");
+        self::assertEquals(200, $response->code);
+        self::assertEquals(null, $response->body);
+    }
+}
