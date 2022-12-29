8a035368a2cd53fe3884e3abf2b6bfaf703c36ae
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2022-12-29 12:24:04 +0100

    Added ProductCategory test.

--- tests/php/integration/SearchTest.php
+++ tests/php/integration/SearchTest.php
@@ -8,6 +8,8 @@ use Relewise\Models\DTO\Currency;
 use Relewise\Models\DTO\DataDoubleSelector;
 use Relewise\Models\DTO\ProductSearchResponse;
 use Relewise\Models\DTO\Language;
+use Relewise\Models\DTO\ProductCategorySearchRequest;
+use Relewise\Models\DTO\ProductCategorySearchResponse;
 use Relewise\Models\DTO\ProductDataRelevanceModifier;
 use Relewise\Models\DTO\ProductSearchRequest;
 use Relewise\Models\DTO\ProductSearchSettings;
@@ -50,5 +52,41 @@ class SearchTest extends TestCase
         self::assertNotEquals(null, $response->body);
         self::assertGreaterThan(0, $productSearchResponse->Hits);
         self::assertNotEmpty($productSearchResponse->Results);
+    }
+
+    
+    public function testProductCategorySearchWithNoConditions(): void
+    {
+        $datasetId = getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
+        $apiKey = getenv('API_KEY') ?: $_ENV['API_KEY'];
+
+        $searcher = new Searcher($datasetId, $apiKey);
+
+        $productCategorySearch = ProductCategorySearchRequest::create()
+            ->withRelevanceModifiers(
+                RelevanceModifierCollection::create()
+                    ->withItems(array(
+                        ProductDataRelevanceModifier::create()
+                            ->withKey("NoveltyBoostModifier")
+                            ->withMultiplierSelector(
+                                DataDoubleSelector::create()
+                                    ->withKey("NoveltyBoostModifier")
+                            )
+                    ))
+            )
+            ->withTake(3)
+            ->withLanguage(Language::create()->withValue("en-US"))
+            ->withCurrency(Currency::create()->withValue("USD"))
+            ->withDisplayedAtLocation("integration test")
+            ->withUser(UserFactory::byTemporaryId("t-Id"));
+
+        $response = $searcher->searchProductCategorySearchRequest($productCategorySearch);
+
+        $productCategorySearchResponse = ProductCategorySearchResponse::hydrate($response->body);
+
+        self::assertEquals(200, $response->code);
+        self::assertNotEquals(null, $response->body);
+        self::assertGreaterThan(0, $productCategorySearchResponse->Hits);
+        self::assertNotEmpty($productCategorySearchResponse->Results);
     }
 }
