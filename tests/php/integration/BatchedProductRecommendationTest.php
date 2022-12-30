660160ab727691d23a21aa0d7c8acd626d8d7c68
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2022-12-30 12:08:13 +0100

    Added Batched Product Recommendation test.

---
+++ tests/php/integration/BatchedProductRecommendationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Relewise\Tests\Integration;
+
+use \PHPUnit\Framework\TestCase;
+use Relewise\Factory\UserFactory;
+use Relewise\Models\DTO\Currency;
+use Relewise\Models\DTO\Language;
+use Relewise\Models\DTO\ProductAndVariantId;
+use Relewise\Models\DTO\ProductRecommendationRequestCollection;
+use Relewise\Models\DTO\ProductRecommendationRequestSettings;
+use Relewise\Models\DTO\ProductsViewedAfterViewingProductRequest;
+use Relewise\Models\DTO\PurchasedWithProductRequest;
+use Relewise\Recommender;
+
+class BatchedProductRecommendationTest extends TestCase
+{
+    public function testBatchedProductRecommendations(): void
+    {
+        $datasetId = getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
+        $apiKey = getenv('API_KEY') ?: $_ENV['API_KEY'];
+
+        $recommender = new Recommender($datasetId, $apiKey);
+
+        $productRecommendationRequestCollection = ProductRecommendationRequestCollection::create()
+            ->withRequests(
+                ProductsViewedAfterViewingProductRequest::create()
+                    ->withSettings(ProductRecommendationRequestSettings::create()->withNumberOfRecommendations(1))
+                    ->withProductAndVariantId(ProductAndVariantId::create()->withProductId("1"))
+                    ->withLanguage(Language::create()->withValue("en-US"))
+                    ->withCurrency(Currency::create()->withValue("USD"))
+                    ->withDisplayedAtLocationType("batched integration test")
+                    ->withUser(UserFactory::byTemporaryId("t-Id")),
+                PurchasedWithProductRequest::create()
+                    ->withSettings(ProductRecommendationRequestSettings::create()->withNumberOfRecommendations(1))
+                    ->withProductAndVariantId(ProductAndVariantId::create()->withProductId("1"))
+                    ->withLanguage(Language::create()->withValue("en-US"))
+                    ->withCurrency(Currency::create()->withValue("USD"))
+                    ->withDisplayedAtLocationType("batched integration test")
+                    ->withUser(UserFactory::byTemporaryId("t-Id"))
+            )
+;
+
+        $response = $recommender->productRecommendationRequestCollection($productRecommendationRequestCollection);
+
+        self::assertNotNull($response);
+        self::assertNotEmpty($response->Responses);
+        self::assertEquals(2, count($response->Responses));
+        self::assertNotEmpty($response->Responses[0]->Recommendations);
+        self::assertNotEmpty($response->Responses[1]->Recommendations);
+    }
+}
