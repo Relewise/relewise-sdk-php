8c74cde398235f5f58bf516cc8dcd44350b539d3
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2022-12-30 11:05:05 +0100

    All Facet Tests pass

--- tests/php/integration/FacetsTest.php
+++ tests/php/integration/FacetsTest.php
@@ -4,12 +4,17 @@ namespace Relewise\Tests\Integration;
 
 use \PHPUnit\Framework\TestCase;
 use Relewise\Factory\UserFactory;
+use Relewise\Models\DTO\BrandFacet;
+use Relewise\Models\DTO\CategoryFacet;
+use Relewise\Models\DTO\CategorySelectionStrategy;
 use Relewise\Models\DTO\Currency;
+use Relewise\Models\DTO\DataSelectionStrategy;
 use Relewise\Models\DTO\FacetingField;
 use Relewise\Models\DTO\FacetSettings;
 use Relewise\Models\DTO\Language;
 use Relewise\Models\DTO\PriceRangeFacet;
 use Relewise\Models\DTO\PriceSelectionStrategy;
+use Relewise\Models\DTO\ProductDataStringValueFacet;
 use Relewise\Models\DTO\ProductFacetQuery;
 use Relewise\Models\DTO\ProductSearchRequest;
 use Relewise\Searcher;
@@ -30,7 +35,6 @@ class FacetsTest extends TestCase
                         PriceRangeFacet::create()
                             ->withField(FacetingField::SalesPrice)
                             ->withPriceSelectionStrategy(PriceSelectionStrategy::Product)
-                            ->withSettings(FacetSettings::create()->withAlwaysIncludeSelectedInAvailable(false))
                     )
             )
             ->withLanguage(Language::create()->withValue("en-us"))
@@ -42,12 +46,110 @@ class FacetsTest extends TestCase
 
         $response = $searcher->productSearchRequest($productSearch);
 
-        fwrite(STDOUT, json_encode($response));
+        self::assertNotNull($response);
+        self::assertNotNull($response->Facets);
+        self::assertNotNull($response->Facets->Items);
+        self::assertNotEmpty($response->Facets->Items);
+        self::assertEquals("Relewise\Models\DTO\PriceRangeFacetResult", get_class($response->Facets->Items[0]));
+        self::assertEquals(FacetingField::SalesPrice, $response->Facets->Items[0]->Field);
+    }
+
+    public function testBrandFacet(): void
+    {
+        $datasetId = getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
+        $apiKey = getenv('API_KEY') ?: $_ENV['API_KEY'];
+
+        $searcher = new Searcher($datasetId, $apiKey);
+
+        $productSearch = ProductSearchRequest::create()
+            ->withFacets(
+                ProductFacetQuery::create()
+                    ->withItems(
+                        BrandFacet::create()
+                            ->withField(FacetingField::Brand)
+                    )
+            )
+            ->withLanguage(Language::create()->withValue("en-us"))
+            ->withCurrency(Currency::create()->withValue("USD"))
+            ->withDisplayedAtLocation("integration test")
+            ->withUser(
+                UserFactory::anonymous()
+            );
+
+        $response = $searcher->productSearchRequest($productSearch);
 
         self::assertNotNull($response);
         self::assertNotNull($response->Facets);
         self::assertNotNull($response->Facets->Items);
         self::assertNotEmpty($response->Facets->Items);
-        self::assertEquals(FacetingField::SalesPrice, $response->Facets->Items[0]->Field);
+        self::assertEquals("Relewise\Models\DTO\BrandFacetResult", get_class($response->Facets->Items[0]));
+        self::assertEquals(FacetingField::Brand, $response->Facets->Items[0]->Field);
+    }
+
+    public function testProductDataFacet(): void
+    {
+        $datasetId = getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
+        $apiKey = getenv('API_KEY') ?: $_ENV['API_KEY'];
+
+        $searcher = new Searcher($datasetId, $apiKey);
+
+        $productSearch = ProductSearchRequest::create()
+            ->withFacets(
+                ProductFacetQuery::create()
+                    ->withItems(
+                        ProductDataStringValueFacet::create()
+                            ->withField(FacetingField::Data)
+                            ->withKey("ShortDescription")
+                            ->withDataSelectionStrategy(DataSelectionStrategy::Product)
+                    )
+            )
+            ->withLanguage(Language::create()->withValue("en-us"))
+            ->withCurrency(Currency::create()->withValue("USD"))
+            ->withDisplayedAtLocation("integration test")
+            ->withUser(
+                UserFactory::anonymous()
+            );
+
+        $response = $searcher->productSearchRequest($productSearch);
+
+        self::assertNotNull($response);
+        self::assertNotNull($response->Facets);
+        self::assertNotNull($response->Facets->Items);
+        self::assertNotEmpty($response->Facets->Items);
+        self::assertEquals("Relewise\Models\DTO\ProductDataStringValueFacetResult", get_class($response->Facets->Items[0]));
+        self::assertEquals(FacetingField::Data, $response->Facets->Items[0]->Field);
+    }
+
+    public function testCategoryFacet(): void
+    {
+        $datasetId = getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
+        $apiKey = getenv('API_KEY') ?: $_ENV['API_KEY'];
+
+        $searcher = new Searcher($datasetId, $apiKey);
+
+        $productSearch = ProductSearchRequest::create()
+            ->withFacets(
+                ProductFacetQuery::create()
+                    ->withItems(
+                        CategoryFacet::create()
+                            ->withField(FacetingField::Category)
+                            ->withCategorySelectionStrategy(CategorySelectionStrategy::ImmediateParent)
+                    )
+            )
+            ->withLanguage(Language::create()->withValue("en-us"))
+            ->withCurrency(Currency::create()->withValue("USD"))
+            ->withDisplayedAtLocation("integration test")
+            ->withUser(
+                UserFactory::anonymous()
+            );
+
+        $response = $searcher->productSearchRequest($productSearch);
+
+        self::assertNotNull($response);
+        self::assertNotNull($response->Facets);
+        self::assertNotNull($response->Facets->Items);
+        self::assertNotEmpty($response->Facets->Items);
+        self::assertEquals("Relewise\Models\DTO\CategoryFacetResult", get_class($response->Facets->Items[0]));
+        self::assertEquals(FacetingField::Category, $response->Facets->Items[0]->Field);
     }
 }
