cadcdc5f53e2d0041663c5413a3b1b4aae4193bf
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-01-10 17:12:08 +0100

    Added CRUD test for Search Index.

--- tests/php/integration/SearchAdministratorTest.php
+++ tests/php/integration/SearchAdministratorTest.php
@@ -4,6 +4,7 @@ namespace Relewise\Tests\Integration;
 
 use Relewise\Models\ClearTextParser;
 use Relewise\Models\DataIndexConfiguration;
+use Relewise\Models\DeleteSearchIndexRequest;
 use Relewise\Models\FieldIndexConfiguration;
 use Relewise\Models\HtmlParser;
 use Relewise\Models\IndexConfiguration;
@@ -14,6 +15,7 @@ use Relewise\Models\PredictionSourceType;
 use Relewise\Models\ProductIndexConfiguration;
 use Relewise\Models\SaveSearchIndexRequest;
 use Relewise\Models\SearchIndex;
+use Relewise\Models\SearchIndexRequest;
 use Relewise\SearchAdministrator;
 
 class SearchAdministratorTest extends BaseTest
@@ -48,5 +50,44 @@ class SearchAdministratorTest extends BaseTest
         $response = $searchAdministrator->saveSearchIndex($request);
 
         self::assertNotNull($response);
+    }
+
+    public function testSaveGetUpdateAndDeleteSearchIndex(): void
+    {
+        $searchAdministrator = new SearchAdministrator($this->DATASET_ID(), $this->API_KEY());
+
+        // Create
+        $saveRequest = SaveSearchIndexRequest::create(
+            SearchIndex::create("to_be_deleted", "Some Description", false)
+                ->setConfiguration(
+                    IndexConfiguration::create()
+                ),
+            "PHP Integration test"
+            );
+        $saveResponse = $searchAdministrator->saveSearchIndex($saveRequest);
+        self::assertNotNull($saveResponse);
+
+        // Read
+        $searchIndexRequest = SearchIndexRequest::create("to_be_deleted");
+        $getResponse = $searchAdministrator->searchIndex($searchIndexRequest);
+        self::assertNotNull($getResponse);
+        self::assertEquals("Some Description", $getResponse->index->description);
+
+        // Udpdate
+        $updateRequest = SaveSearchIndexRequest::create(
+            SearchIndex::create("to_be_deleted", "Another Description", false)
+                ->setConfiguration(
+                    IndexConfiguration::create()
+                ),
+            "PHP Integration test"
+            );
+        $updateResponse = $searchAdministrator->saveSearchIndex($updateRequest);
+        self::assertNotNull($updateResponse);
+        self::assertEquals("Another Description", $updateResponse->index->description);
+
+        // Delete
+        $searchIndexRequest = DeleteSearchIndexRequest::create("to_be_deleted", "PHP Integration test");
+        $deleteResponse = $searchAdministrator->deleteSearchIndex($searchIndexRequest);
+        self::assertNull($deleteResponse);
     }
 }
