920fdc8cfcb0877896d06b3e0e2ac215453d0f9f
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-02-08 11:31:14 +0100

    Added test to BaseTestCase

--- tests/php/integration/BaseTestCase.php
+++ tests/php/integration/BaseTestCase.php
@@ -5,9 +5,15 @@ use PHPUnit\Framework\TestCase;
 
 class BaseTestCase extends TestCase
 {
+    public function testGetDatasetIdAndApiKey(): void
+    {
+        self::assertNotNull($this->DATASET_ID());
+        self::assertNotNull($this->API_KEY());
+    }
+
     public function DATASET_ID() : string
     {
-        return  getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
+        return getenv('DATASET_ID') ?: $_ENV['DATASET_ID'];
     }
 
     public function API_KEY() : string
