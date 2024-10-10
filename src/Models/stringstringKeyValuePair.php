9ada81ada54eb7f55a6265f9024ac41e8c327661
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2024-10-10 10:30:43 +0200

    Added spacing in key-value-pair writer.

--- src/Models/stringstringKeyValuePair.php
+++ src/Models/stringstringKeyValuePair.php
@@ -7,16 +7,20 @@ use DateTime;
 class stringstringKeyValuePair
 {
     public string $key;
+    
     public string $value;
+    
     function __construct(string $key, string $value)
     {
         $this->key = $key;
         $this->value = $value;
     }
+    
     public static function create(string $key, string $value) : stringstringKeyValuePair
     {
         return new stringstringKeyValuePair($key, $value);
     }
+    
     public static function hydrate(array $arr) : stringstringKeyValuePair
     {
         return stringstringKeyValuePair::create($arr["key"], $arr["value"]);
