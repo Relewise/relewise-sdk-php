dee3e27727c14d9af906bce1e9f1ffe0a14083dc
Simon Walenkamp Hansen <swh@relewise.com>
2025-04-15 15:36:26 +0200

    new recently purches facet model

--- src/Models/RecentlyPurchasedFacet.php
+++ src/Models/RecentlyPurchasedFacet.php
@@ -8,7 +8,7 @@ class RecentlyPurchasedFacet extends boolValueFacet
     public string $typeDefinition = "Relewise.Client.DataTypes.Search.Facets.Queries.RecentlyPurchasedFacet, Relewise.Client";
     public PurchaseQualifiers $purchaseQualifiers;
     
-    public static function create(PurchaseQualifiers $purchaseQualifiers, bool ... $selected = Null) : RecentlyPurchasedFacet
+    public static function create(PurchaseQualifiers $purchaseQualifiers, bool ... $selected) : RecentlyPurchasedFacet
     {
         $result = new RecentlyPurchasedFacet();
         $result->purchaseQualifiers = $purchaseQualifiers;
